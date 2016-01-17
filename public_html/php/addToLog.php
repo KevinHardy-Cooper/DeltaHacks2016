25ce660fb404c7371d0ebb56a77098023dd10e2c
Eric Lefort <leforte@mcmaster.ca>
2016-01-17 08:01:22 -0500

    Adding messages complete

--- public_html/php/addToLog.php
+++ public_html/php/addToLog.php
@@ -1,27 +1,20 @@
 <?php
-	$username = $_GET['username'];
-	$message = $_GET['message'];
-
-	$findFriend = "SELECT * FROM `2035081_letstalk`.`Log` WHERE `ListenerEmail` = '" . $username . "' OR `TalkerEmail` = '". $username ."';";
+    $username = $_GET['username'];
+    $message = $_GET['message'];
+        
+        $con = mysqli_connect("fdb3.awardspace.net","2035081_letstalk","SimplyCoded1","2035081_letstalk","3306");
+        
+        $findFriend = "SELECT * FROM `2035081_letstalk`.`Log` WHERE `ListenerEmail` = '" . $username . "' OR `TalkerEmail` = '". $username ."';";
     $testResult = mysqli_query($con, $findFriend);
-
+    
     if (mysqli_num_rows($testResult) > 0) {
-        $row = mysqli_fetch_assoc($testResult)
+        $row = mysqli_fetch_assoc($testResult);
         $listener = $row["ListenerEmail"];
         $talker = $row["TalkerEmail"];
     }
-
-	$con = mysqli_connect("fdb3.awardspace.net","2035081_letstalk","SimplyCoded1","2035081_letstalk","3306");
-    $sql = "INSERT INTO `2035081_letstalk`.`Log` VALUES ('". $listener ."','". $talker ."','". $username ."','". $message ."');";
+    
+            $sql = "INSERT INTO `2035081_letstalk`.`Log`(`ListenerEmail`,`TalkerEmail`, `SenderEmail`, `Message`) VALUES ('". $listener ."','". $talker ."','". $username ."','". $message ."');";
+    echo $sql;
     // Check connection
-    if (mysqli_connect_errno()) {
-        echo "Fail";
-    }
     $result = mysqli_query($con, $sql);
-
-    if($result) {
-    	echo "Added!";
-    } else {
-    	echo "Not Added... :(";
-    }
 ?>
