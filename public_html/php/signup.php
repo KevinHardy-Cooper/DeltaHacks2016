39525f5e5b6042c6de31a7067ef9ef8b847fc3ba
Eric Lefort <leforte@mcmaster.ca>
2016-01-17 00:55:58 -0500

    Beginning of Round 3 file synchronization

--- public_html/php/signup.php
+++ public_html/php/signup.php
@@ -1,85 +1,4 @@
 <?php
-
-/* 
-Script:
-
-$(#signUpButton).click(function (){
-	//Grab all data
-	var fName = $(#firstName).val();	
-	var lName = $(#lastName).val();	
-	var email = $(#email).val();	
-	var password = $(#password).val();
-	var username; //TODO Grab from slider/selector?
-	
-	var aNum = $(#addressNumber).val();
-	var aStr = $(#addressStreet).val();
-	var aCit = $(#addressCity).val();
-	var aCod = $(#addressCode).val();
-	var aReg = $(#addressRegion).val();
-	var aCou = $(#addressCountry).val();
-
-	var signUpAch = $(#signUpAchievement).val();
-
-	var gender;
-	if($("#male").attr("checked")) {
-		gender = "M";
-	} else if($("#female").attr("checked")) {
-		gender = "F";
-	} else if($("#other").attr("checked")) {
-		gender = "O";
-	} else {
-		//TODO Spencer Notify User that northing was clicked, but needs to be.
-	}
-	
-	if(fName.length < 1 || fName.length > 255) {
-		moveToError("First Name");
-	} else if(lName.length < 1 || lName.length > 255) {
-		moveToError("Last Name");
-	} else if(password.length < 7 || password.length > 32) {
-		moveToError("Password");
-	} else if(aNum.length < 1 || aNum.length > 9) {
-		moveToError("Address Number");
-	} else if(aStr.length < 1 || aStr.length > 255) {
-		moveToError("Address Street");
-	} else if(aCit.length < 1 || aCit.length > 255) {
-		moveToError("Address City");
-	} else if(aCod.length < 1 || aCod.length > 6) {
-		moveToError("Address Code");
-	} else if(aReg.length < 1 || aReg.length > 255) {
-		moveToError("Postal Code/Zip Code");
-	} else if(aCou.length < 4 || aCou.length > 35) {
-		moveToError("Country Code");
-	} else if(signUpAch.length < 5 || signUpAch.length > 140) {
-		moveToError("Sign Up Architecture");
-	} 
-	//TODO PHP Validate email
-	//TODO Verify Address
-	//TODO Send Valid stuff to PHP
-
-	$.ajax({
-			method: 'get',
-  			url: 'php/signUpScript.php',
-  			data: {
-    			'fname': fname,
-    			'lname': lname,
-    			'email': email,
-    			'password': password,
-    			'aNum': aNum,
-    			'aStr': aStr,
-    			'aCit': aCit,
-    			'aCod': aCod,
-    			'aReg': aReg,
-    			'aCou': aCou,
-    			'signUpAch': signUpAch,
-    			'username': username
-  			},
-  			success: function(msg) {
-
-  			}
-		});
-});
-
-*/
 	$username = $_GET['usernameNoun'];
 	$username = $_GET['usernameAdjective'];
 	$fName = $_GET['fName'];
@@ -95,13 +14,14 @@ $(#signUpButton).click(function (){
 	$signUpAch = $_GET['signUpAch'];
 	$gender = $_GET['gender'];
 
-	$personInsertCmd = "INSERT INTO `2035081_letstalk`.`Person` VALUES (`" . $email . "`, `" . $fName . "`, `" . $lName . "`, `" . $password . 
-		"`, `" . $usernameNoun. "`, `" . $usernameAdjective . "`, `" . $gender . "`, `" . $signUpAch . "`, `" . $aCod . "`, `" . $aStr .
-		"`, `" . $aNum . "`, `" . $aCit . "`, `" . $aReg . "`, `" . $aCou . "`)";
+	$personInsertCmd = "INSERT INTO `2035081_letstalk`.`Person` VALUES ('" . $email . "', '" . $fName . "', '" . $lName . "', '" . $password . 
+		"', '" . $usernameNoun. "', '" . $usernameAdjective . "', '" . $gender . "', '" . $signUpAch . "', '" . $aCod . "', '" . $aStr .
+		"', '" . $aNum . "', '" . $aCit . "', '" . $aReg . "', '" . $aCou . "');";
 
+	//(`Email`, `FirstName`, `LastName`, `Password`, `UserAdjective`, `UserNoun`, `Gender`, `SmallAchievement`, `PostalCode`, `StreetName`, `StreetNum`, `City`, `Region`, `Country`)
 	//TODO Implement Listener functionality.
-	//$listenerInsertCmd = "INSERT INTO `2035081_letstalk`.`Listener` VALUES (`" . $email . "`, `" . $solver . "`, `" . $listenExp . "`, `" .
-	//	$personalExp . "`, `" . $bluntness . "`);";
+	//$listenerInsertCmd = "INSERT INTO `2035081_letstalk`.`Listener` VALUES ('" . $email . "', '" . $solver . "', '" . $listenExp . "', '" .
+	//	$personalExp . "', '" . $bluntness . "');";
 
 	$con = mysqli_connect("fdb3.awardspace.net","2035081_letstalk","SimplyCoded1","2035081_letstalk","3306");
 
@@ -109,8 +29,5 @@ $(#signUpButton).click(function (){
 	if (mysqli_connect_errno()) {
   		echo "Fail";
   	}
-	$result = mysqli_query($con, $personInsertCmd);
-
-
+	mysqli_query($con, $personInsertCmd);
 ?>
-
--- public_html/php/verify.php
+++ public_html/php/verify.php
@@ -1,36 +1,34 @@
 <?php
-	$username = $_GET['username'];
-	$password = $_GET['password'];
-	$con = mysqli_connect("fdb3.awardspace.net","2035081_letstalk","SimplyCoded1","2035081_letstalk","3306");
+  session_start();
+  $username = $_GET['username'];
+  $password = $_GET['password'];
+  $con = mysqli_connect("fdb3.awardspace.net","2035081_letstalk","SimplyCoded1","2035081_letstalk","3306");
 
-	// Check connection
-	if (mysqli_connect_errno()) {
-  		echo "Fail";
-  	}
+  // Check connection
+  if (!$con) {
+    echo "Fail";
+  }
 
-  	if(strpos($username,' ') !== false) {
-  		$userNamePieces = explode(" ", $username);
-		$sql = "SELECT * FROM `Person` WHERE `UsernameAdjective` = '" . $userNamePieces[0] . "' AND `` = '" . $userNamePieces[1] . "';";
-		$result = mysqli_query($con, $sql);
-  	} else {
-  		//TODO Check if it is a valid email address
-  		$sql = "SELECT * FROM `Person` WHERE `Email` = '" . $username . "';";
-  		$result = mysqli_query($con, $sql);
-  	}
+  if(strpos($username,' ') !== false) {
+    $userNamePieces = explode(" ", $username);
+    $sql = "SELECT * FROM `2035081_letstalk`.`Person` WHERE `UsernameAdjective` = '" . $userNamePieces[0] . "' AND `` = '" . $userNamePieces[1] . "';";
+    $result = mysqli_query($con, $sql);
+  } else {
+    //TODO Check if it is a valid email address
+    $sql = "SELECT * FROM `2035081_letstalk`.`Person` WHERE `Email` = '" . $username . "';";
+    $result = mysqli_query($con, $sql);
+  }
 
-	if (mysqli_num_rows($result) > 0) {							//Did we get a result?
-    	while($row = mysqli_fetch_assoc($result)) {		
-        	$hashed = $row["Password"];							//Get the Hashed Password
-        	if (($hashed == crypt($password, $hashed))) {		//Check against input password
-  				echo "True";
-  				session_start();
-  				$_SESSION["Username"] = $username;									
-			} else {
-				echo "False";
-			}
-    	}
-	} else {
-		echo "DNE";
-	}
-
+  if (mysqli_num_rows($result) > 0) {             //Did we get a result?
+    $row = mysqli_fetch_assoc($result);
+    $hashed = $row["Password"];             //Get the Hashed Password
+    if (($hashed == crypt($password, $hashed))) {   //Check against input password
+      echo "True";
+      $_SESSION["Username"] = $username;                
+    } else {
+      echo "False";
+    }
+  } else {
+    echo "DNE";
+  }
 ?>
