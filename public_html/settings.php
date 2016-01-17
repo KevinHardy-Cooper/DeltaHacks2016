2e275d09052547565f156ddc9f28f587734a2382
Spencer Deevy <deevy.spencer@gmail.com>
2016-01-17 10:41:43 -0500

    jinjinjn

--- public_html/settings.php
+++ public_html/settings.php
@@ -88,27 +88,27 @@
                 <h1><i class="fa fa-circle-o fa-5x"></i></h1>
                 <hr>
                 <p class="smallAchievement" id="smallAchievement"> SmallAchievement: </p>
-                <p class="editable" id="smallAchievementEdit"><?php echo $Person["SmallAchievement"] ?> </p>
+                <p class="editable" id="smallAchievementEdit"><?php echo $person["SmallAchievement"] ?> </p>
                  <p class="desciption" id="description"> Description: </p>
-                 <p class="editable" id="description" ><?php echo $Person["Description"] ?> </p>
+                 <p class="editable" id="description" ><?php echo $person["Description"] ?> </p>
                 
             </div>
         </div>
     </header>
 <div class="tableHeading">
 <p>First Name:</p>
-<p class="editable" id="firstNameEdit"><?php echo $Person["FirstName"]?></p>
+<p class="editable" id="firstNameEdit"><?php echo $person["FirstName"]?></p>
 <p>Last Name:</p>
-    <p class="editable" id="lastNameEdit"><?php echo $Person["LastName"]?></p>
+    <p class="editable" id="lastNameEdit"><?php echo $person["LastName"]?></p>
             <p id="editButton1"  class="btn btn-default" ><i class="fa fa-pencil"></i></p>
 
 </div>
 
 <div class="tableHeading1">
 <p>Gender:</p>
-    <p class="editable" id="gender"><?php echo $Person["Gender"]?></p>
+    <p class="editable" id="gender"><?php echo $person["Gender"]?></p>
 <p>Email:</p>
-    <p id="email"><?php echo $Person["Email"]?></p>
+    <p id="email"><?php echo $person["Email"]?></p>
              <p id="editButton2"  class="btn btn-default" ><i class="fa fa-pencil"></i></p>
 
 </div>
