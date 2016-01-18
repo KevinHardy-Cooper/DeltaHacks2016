2694a4eb5e9ae3660b5c8eba2535e793261cb37d
Eric Lefort <leforte@mcmaster.ca>
2016-01-18 13:40:50 -0500

    Last changes before end of hack, small sql tweak

--- public_html/php/getMessages.php
+++ public_html/php/getMessages.php
@@ -3,7 +3,7 @@ $username = $_GET['username'];
 $lastSynced = $_GET['lastDate'];
 $con = mysqli_connect("fdb3.awardspace.net","2035081_letstalk","SimplyCoded1","2035081_letstalk","3306");
 
-$sql = "SELECT * FROM `2035081_letstalk`.`Log` WHERE (`ListenerEmail` = '". $username ."' OR `TalkerEmail` = ' ". $username ." ') AND `TimeStamp` > '" . $lastSynced . "';" ;
+$sql = "SELECT DISTINCT * FROM `2035081_letstalk`.`Log` WHERE (`ListenerEmail` = '". $username ."' OR `TalkerEmail` = ' ". $username ." ') AND `TimeStamp` > '" . $lastSynced . "';" ;
 
 $result = mysqli_query($con,$sql);
 if (mysqli_num_rows($result) > 0) {
