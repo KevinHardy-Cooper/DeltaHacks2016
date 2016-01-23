470da230f6392ff00680e33d45c698615bda9a8d
Eric Lefort <leforte@mcmaster.ca>
2016-01-23 12:59:07 -0500

    Fixed the verifyUser.php sql statement

--- public_html/php/verifyUser.php
+++ public_html/php/verifyUser.php
@@ -10,7 +10,7 @@
 
   if(strpos($username,' ') !== false) {
     $userNamePieces = explode(" ", $username);
-    $sql = "SELECT * FROM `LetsTalk`.`Person` WHERE `UsernameAdjective` = '" . $userNamePieces[0] . "' AND `` = '" . $userNamePieces[1] . "';";
+    $sql = "SELECT * FROM `LetsTalk`.`Person` WHERE `UsernameAdjective` = '" . $userNamePieces[0] . "' AND `UsernameNoun` = '" . $userNamePieces[1] . "';";
     $result = mysqli_query($con, $sql);
   } else {
     //TODO Check if it is a valid email address
@@ -18,9 +18,9 @@
     $result = mysqli_query($con, $sql);
   }
 
-  if (mysqli_num_rows($result) > 0) {             //Did we get a result?
+  if (mysqli_num_rows($result) > 0) {               //Did we get a result?
     $row = mysqli_fetch_assoc($result);
-    $hashed = $row["Password"];             //Get the Hashed Password
+    $hashed = $row["Password"];                     //Get the Hashed Password
     if (($hashed == crypt($password, $hashed))) {   //Check against input password
       echo "True";
       $_SESSION["Username"] = $username;
